441621e28a5e51ffd310b68fd91c8d57342f4dd3
Konstantin <kruglovkn90@gmail.com>
2018-04-04 00:37:40 +0300

    добавлены методы для определиния площадей квадрата и прямоугольника

---
+++ sandbox/src/main/java/ru/stqa/krug/sandbox/Rectangle.java
@@ -0,0 +1,14 @@
+package ru.stqa.krug.sandbox;
+
+public class Rectangle {
+    double a;
+    double b;
+
+    public Rectangle (double a, double b){
+        this.a= a;
+        this.b = b;
+    }
+    public double area (){
+        return this.a * this.b;
+    }
+}
