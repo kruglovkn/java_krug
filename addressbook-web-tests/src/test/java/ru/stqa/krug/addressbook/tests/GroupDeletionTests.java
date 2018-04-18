9fa19a758c307c60db75253e39648f0ef244bbf5
Konstantin <kruglovkn90@gmail.com>
2018-04-18 04:10:50 +0300

    создана проверка условий для удаления групп

--- addressbook-web-tests/src/test/java/ru/stqa/krug/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/krug/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.krug.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.krug.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
@@ -9,6 +10,10 @@ public class GroupDeletionTests extends TestBase {
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().goToGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()){
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
+
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
