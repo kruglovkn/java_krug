53bc634c208d2a8dd7e27e14f9df6eadb1900f42
Konstantin <kruglovkn90@gmail.com>
2018-05-29 21:21:42 +0300

    лекция 8.7 недоработанная

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -16,8 +16,6 @@ public class RegistrationHelper extends HelperBase{
         type(By.name("username"),username);
         type(By.name("email"), email);
         click(By.xpath("//input[@value='Signup']"));
-
-
     }
 
     public void finish(String conformationLink, String password) {
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -14,17 +14,20 @@ import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase {
 
-    @BeforeMethod
+   // @BeforeMethod
     public void startMailServer() {
         app.mail().start();
     }
     @Test
     public void testRegistration() throws IOException, MessagingException {
-        String user = "user2";
+        long now = System.currentTimeMillis();
+        String user = String.format("user%s", now);
         String password = "password";
-        String email = "user@localhost.localdomain";
+        String email = String.format("user%s@localhost.localdomain", now);
+        app.james().createUser(user, password);
         app.registration().start(user, email);
-        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        //List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
         String conformationLink = findConformationLink(mailMessages, email);
         app.registration().finish(conformationLink, password);
 
@@ -34,11 +37,11 @@ public class RegistrationTests extends TestBase {
 
     private String findConformationLink(List<MailMessage> mailMessages, String email) {
         MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
-        VerbalExpression regex = VerbalExpression.regex().find("http://*").nonSpace().oneOrMore().build();
+        VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
         return regex.getText(mailMessage.text);
     }
 
-    @AfterMethod (alwaysRun = true)
+  //  @AfterMethod (alwaysRun = true)
     public void stopMailServer() {
         app.mail().stop();
     }
