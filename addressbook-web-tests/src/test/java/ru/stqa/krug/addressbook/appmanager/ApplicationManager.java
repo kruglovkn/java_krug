86d554a5608a3e3baf282b7c9aa2cae88016ba58
Konstantin <kruglovkn90@gmail.com>
2018-06-02 16:54:51 +0300

    подключен selenium server

--- addressbook-web-tests/src/test/java/ru/stqa/krug/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/krug/addressbook/appmanager/ApplicationManager.java
@@ -1,15 +1,19 @@
 package ru.stqa.krug.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -35,13 +39,18 @@ public class ApplicationManager {
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
         dbHelper = new DbHelper();
 
-
-        if (browser.equals(org.openqa.selenium.remote.BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        } else if (browser.equals(org.openqa.selenium.remote.BrowserType.CHROME)){
-            wd = new ChromeDriver();
-        } else if (browser.equals(org.openqa.selenium.remote.BrowserType.EDGE)) {
-            wd = new EdgeDriver();
+        if("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(org.openqa.selenium.remote.BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+            } else if (browser.equals(org.openqa.selenium.remote.BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(org.openqa.selenium.remote.BrowserType.EDGE)) {
+                wd = new EdgeDriver();
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
 
 
